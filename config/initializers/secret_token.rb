46a4efc14728e41dfa63988df868dc719122bcf1
reiz <robert.reiz.81@gmail.com>
2012-09-07 20:55:48 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unfucktheplanet::Application.config.secret_token = '73f3a70cf639ea0ab552a749475b4de6697ac095633c7507c1c8a556648bb51b17edf89813fab6b7d2798a521de6a4382ddf10ea28a8d95b7ca45f7cebe0aba2'
