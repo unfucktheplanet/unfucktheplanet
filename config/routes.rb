78ca2645c0b57e1ccbb2f848171ae4390e9d505f
Jossekin Beilharz <beilharz@acm.org>
2012-09-23 11:00:48 +0200

    added the rails resources as routes so that /fucks.json works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Unfucktheplanet::Application.routes.draw do
 
   root to: 'home#index'
+  
+  resources :users
+  resources :locations
+  resources :fucks
+  resources :unfucks
+  resources :resources
+  resources :notifications
 
 end
